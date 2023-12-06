b42058f93e27ece3b7ce37e1b03887ba2e1c2a7c
Barbara Ruiz <barbararuiz456@outlook.com>
2023-12-05 21:54:37 -0600

    done system, done style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     @question = params.dig(:question)
 
     if @question[-1] == "?"
-      @answer = "Silly question, get dressed and go to work!."
+      @answer = "Silly question, get dressed and go to work!"
     elsif @question == "I am going to work"
       @answer = "Great!"
     else
